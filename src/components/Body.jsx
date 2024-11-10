af18bf6f90c57bae846c9e19fd80e457b4380d23
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-11-10 10:48:03 +0530

    navbar added

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Body = () => {
+  return (
+    <div className="bg-black w-full h-screen px-0 sm:px-32 md:px-44 ">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Body;
